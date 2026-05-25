dd7eac8cbe98206703a14a5f61c70e3e5adbc82d
PattryckS <patrickferreira1109@gmail.com>
2026-05-25 09:54:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Envie suas fotos aqui! 📸")
+
+# Botão para upload de múltiplos arquivos
+fotos = st.file_uploader("Selecione todas as fotos", accept_multiple_files=True, type=["jpg", "png", "jpeg"])
+
+if st.button("Enviar Fotos"):
+    if fotos:
+        for foto in fotos:
+            # Salva o arquivo original na pasta do servidor
+            with open(os.path.join("pasta_recebidos", foto.name), "wb") as f:
+                f.write(foto.getbuffer())
+        st.success("Todas as fotos foram enviadas com sucesso e qualidade original!")
+    else:
+        st.error("Por favor, selecione pelo menos uma foto.")
